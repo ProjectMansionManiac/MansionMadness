0ff05ac766a821a6ea6b9dcc4a83ea8167eeaae4
Injec <rick.junge@web.de>
2018-06-04 14:07:26 +0200

    Boss Phase Implementet
    [+] Phase System
    [+] Phase 1 (mirror)

---
+++ Assets/Scripts/BossPhaseMirror.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossPhaseMirror : BossPhase {
+    [SerializeField] private GameObject[] mirrors;
+
+	public override void ActivatePhase()
+    {
+
+        torso.SetActive(true);
+
+        foreach (GameObject mirror in mirrors)
+        {
+            mirror.SetActive(true);
+        }
+    }
+
+    public override void DeactivatePhase()
+    {
+        base.DeactivatePhase();
+
+        foreach (GameObject mirror in mirrors)
+        {
+            mirror.SetActive(false);
+        }
+    }
+
+    private void Update()
+    {
+
+    }
+}
