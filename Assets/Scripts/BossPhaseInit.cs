ef62599fcb4d2ceda27a558745aaa8d359c901e3
Injec <rick.junge@web.de>
2018-06-05 17:41:57 +0200

    Boss Phase Controller
    [+] Boss Phase Controller

--- Assets/Scripts/BossPhaseInit.cs
+++ Assets/Scripts/BossPhaseInit.cs
@@ -4,13 +4,44 @@ using UnityEngine;
 
 public class BossPhaseInit : BossPhase {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    [Header("Health fraction from 0-1. The phase will change at this health fraction")]
+    public float healthFraction;
+
+    public override void ActivatePhase()
+    {
+        base.ActivatePhase();
+        head.tag = "Enemy";
+        torso.tag = "Enemy";
+        rightArm.tag = "Enemy";
+        leftArm.tag = "Enemy";
+        rightLeg.tag = "Enemy";
+        leftLeg.tag = "Enemy";
+
+        //animator.Play("InitPhase");
+    }
+
+    private void OnEnable()
+    {
+        ActivatePhase();
+    }
+
+    public override void DeactivatePhase()
+    {
+        base.DeactivatePhase();
+    }
+
+    private void Update()
+    {
+        totalHealth = 0;
+        foreach (var damageComponent in allDamageComponents)
+        {
+            totalHealth += (int)damageComponent.health;
+        }
+        if (totalHealth / totalMaxHealth < healthFraction)
+        {
+            DeactivatePhase();
+            PhaseController.instance.StartNextPhase();
+            this.enabled = false;
+        }
+    }
 }
---
+++ Assets/Scripts/PhaseController.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhaseController : MonoBehaviour {
+
+    public PhaseType[] phases;
+    int currentPhaseIndex = 0;
+
+    public static PhaseController instance = null;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    private void Start()
+    {
+        StartNextPhase();
+    }
+
+    public enum PhaseType
+    {
+        Init,
+        Mirror,
+        Spear,
+        MirrorSpear,
+        Charge,
+        Projectile,
+        ChargeProjectile,
+        All
+    }
+
+    public void StartNextPhase()
+    {
+        switch (currentPhaseIndex)
+        {
+            case (int)PhaseType.Init:
+                GetComponent<BossPhaseInit>().enabled = true;
+                break;
+            case (int)PhaseType.Mirror:
+                GetComponent<BossPhaseMirror>().enabled = true;
+                break;
+            case (int)PhaseType.Spear:
+                GetComponent<BossPhaseSpear>().enabled = true;
+                break;
+            case (int)PhaseType.MirrorSpear:
+
+                break;
+            case (int)PhaseType.Charge:
+
+                break;
+            case (int)PhaseType.Projectile:
+
+                break;
+            case (int)PhaseType.ChargeProjectile:
+
+                break;
+            case (int)PhaseType.All:
+
+                break;
+        }
+
+        currentPhaseIndex++;
+    }
+}
