5d472a950dd1fa269cccbbd25c211024725ca691
Patrick Kurras <VisualBasic@hotmail.de>
2018-06-06 14:44:49 +0200

    Spear functionality #2
    [!] forgot to add actual spear class and countdown ... did that now. R.I.P.

---
+++ Assets/Scripts/Countdown.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using UnityEngine;
+
+public class Countdown
+{
+    private Action action;
+    private float tickRate;
+    private float currentTime;
+
+    private bool activated;
+
+    public Countdown(Action action, float tickRate)
+    {
+        this.action = action;
+        this.tickRate = tickRate;
+        this.currentTime = tickRate;
+        this.activated = false;
+    }
+
+    public void Update(float deltaTime, bool log)
+    {
+        if (activated)
+            return;
+
+        if (log)
+        {
+            Debug.Log("Delta Time: " + deltaTime);
+            Debug.Log("Current Time: " + currentTime);
+        }
+
+        if ((this.currentTime - deltaTime) <= 0)
+        {
+            this.currentTime = this.tickRate;
+            Tick();
+        }
+
+        this.currentTime -= deltaTime;
+    }
+    private void Tick()
+    {
+        action();
+        activated = true;
+    }
+}
