e61a20ab81e3c2af4b205710cc3cbab2423168bb
Patrick Kurras <VisualBasic@hotmail.de>
2018-06-07 12:36:13 +0200

    Music Zone
    [+] added functionality for music zones
    [+] added boss room music zone
    [-] removed the audio source from the tilemap and added it to the player

---
+++ Assets/Scripts/MusicZoneComponent.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicZoneComponent : MonoBehaviour
+{
+    [SerializeField]
+    private AudioClip audioClip;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            var audioSource = collision.gameObject.GetComponent<AudioSource>();
+
+            if (audioSource == null)
+                return;
+
+            audioSource.Stop();
+            audioSource.clip = audioClip;
+            audioSource.Play();
+        }
+    }
+}
