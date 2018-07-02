976813c3f1e30df7c8f5ef467f05a77f76cadfa6
Injec <rick.junge@web.de>
2018-07-02 18:33:22 +0200

    Duck fix and camera fix
    [*] fixed duck
    [*] camera FIX

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -2,123 +2,29 @@
 
 public class CameraFollow : MonoBehaviour
 {
-    public Controller2D target;
-    public float verticalOffset;
-    public float lookAheadDstX;
-    public float lookSmoothTimeX;
-    public float verticalSmoothTime;
-    public Vector2 focusAreaSize;
-
-    private FocusArea focusArea;
-
-    private float currentLookAheadX;
-    private float targetLookAheadX;
-    private float lookAheadDirX;
-    private float smoothLookVelocityX;
-    private float smoothVelocityY;
-
-    private bool lookAheadStopped;
-
     [SerializeField] private Vector2 levelTopLeft;
     [SerializeField] private Vector2 levelBottomRight;
-
     [SerializeField] private float cameraClipOffsetX;
     [SerializeField] private float cameraClipOffsetY;
 
-    private void Start()
+    public float dampTime = 0.15f;
+    public Transform target;
+
+    void LateUpdate()
     {
-        focusArea = new FocusArea(target.coll.bounds, focusAreaSize);
-    }
-
-    private void LateUpdate()
-    {
-        focusArea.Update(target.coll.bounds);
-
-        Vector2 focusPosition = focusArea.centre + Vector2.up * verticalOffset;
-
-        if (focusArea.velocity.x != 0)
+        if (target)
         {
-            lookAheadDirX = Mathf.Sign(focusArea.velocity.x);
-            if (Mathf.Sign(target.playerInput.x) == Mathf.Sign(focusArea.velocity.x) && target.playerInput.x != 0)
-            {
-                lookAheadStopped = false;
-                targetLookAheadX = lookAheadDirX * lookAheadDstX;
-            }
-            else
-            {
-                if (!lookAheadStopped)
-                {
-                    lookAheadStopped = true;
-                    targetLookAheadX = currentLookAheadX + (lookAheadDirX * lookAheadDstX - currentLookAheadX) / 4f;
-                }
-            }
+            Vector3 from = transform.position;
+            Vector3 to = target.position;
+            to.z = transform.position.z;
+
+            transform.position -= (from - to) * dampTime * Time.deltaTime;
         }
-
-        currentLookAheadX = Mathf.SmoothDamp(currentLookAheadX, targetLookAheadX, ref smoothLookVelocityX, lookSmoothTimeX);
-
-        focusPosition.y = Mathf.SmoothDamp(transform.position.y, focusPosition.y, ref smoothVelocityY, verticalSmoothTime);
-        focusPosition += Vector2.right * currentLookAheadX;
-        transform.position = (Vector3)focusPosition + Vector3.forward * -10;
 
         transform.position = new Vector3(
-            Mathf.Clamp(transform.position.x, levelTopLeft.x + cameraClipOffsetX, levelBottomRight.x - cameraClipOffsetX),
-            Mathf.Clamp(transform.position.y, levelBottomRight.y + cameraClipOffsetY, levelTopLeft.y - cameraClipOffsetY),
-            transform.position.z
-        );
-    }
-
-    private void OnDrawGizmos()
-    {
-        Gizmos.color = new Color(1, 0, 0, .5f);
-        Gizmos.DrawCube(focusArea.centre, focusAreaSize);
-    }
-
-    private struct FocusArea
-    {
-        public Vector2 centre;
-        public Vector2 velocity;
-        private float left, right;
-        private float top, bottom;
-
-        public FocusArea(Bounds targetBounds, Vector2 size)
-        {
-            left = targetBounds.center.x - size.x / 2;
-            right = targetBounds.center.x + size.x / 2;
-            bottom = targetBounds.min.y;
-            top = targetBounds.min.y + size.y;
-
-            velocity = Vector2.zero;
-            centre = new Vector2((left + right) / 2, (top + bottom) / 2);
-        }
-
-        public void Update(Bounds targetBounds)
-        {
-            float shiftX = 0f;
-            if (targetBounds.min.x < left)
-            {
-                shiftX = targetBounds.min.x - left;
-            }
-            else if (targetBounds.max.x > right)
-            {
-                shiftX = targetBounds.max.x - right;
-            }
-            left += shiftX;
-            right += shiftX;
-
-            float shiftY = 0f;
-            if (targetBounds.min.y < bottom)
-            {
-                shiftY = targetBounds.min.y - bottom;
-            }
-            else if (targetBounds.max.y > top)
-            {
-                shiftY = targetBounds.max.y - top;
-            }
-            top += shiftY;
-            bottom += shiftY;
-
-            centre = new Vector2((left + right) / 2, (top + bottom) / 2);
-            velocity = new Vector2(shiftX, shiftY);
-        }
+           Mathf.Clamp(transform.position.x, levelTopLeft.x + cameraClipOffsetX, levelBottomRight.x - cameraClipOffsetX),
+           Mathf.Clamp(transform.position.y, levelBottomRight.y + cameraClipOffsetY, levelTopLeft.y - cameraClipOffsetY),
+           transform.position.z
+       );
     }
 }
