ecf1b6878f869790c85dcd7cb0f975dc0cd7a1f7
Injec <rick.junge@web.de>
2018-07-04 13:39:02 +0200

    Spike ceiling fix
    [*] spike not colliding with the ceiling anymore

--- Assets/Scripts/BossRoomTrigger.cs
+++ Assets/Scripts/BossRoomTrigger.cs
@@ -5,11 +5,15 @@ using UnityEngine;
 public class BossRoomTrigger : MonoBehaviour
 {
 
+    public GameObject doorToSpawn;
+
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.name == "Player")
         {
             PhaseController.instance.StartNextPhase();
+            doorToSpawn.SetActive(true);
             Destroy(this.gameObject);
         }
     }
