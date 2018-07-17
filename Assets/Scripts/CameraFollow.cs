4eb7ad761cd07326bddcb5d2daa3d01adc3f06b6
Injec <rick.junge@web.de>
2018-07-17 15:45:15 +0200

    Camera Follow semifinal
    [*] camera follows point between player and enemy, if player is close to enemy.

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -6,19 +6,40 @@ public class CameraFollow : MonoBehaviour
     [SerializeField] public Vector2 levelBottomRight;
     [SerializeField] private float cameraClipOffsetX;
     [SerializeField] private float cameraClipOffsetY;
+    float distanceBetweenTargets;
+    public float maxDistanceZ, minDistanceZ;
 
     public float dampTime = 0.15f;
-    public Transform target;
+    public Vector3 target;
+
+    public Transform enemyTransform, playerTransform;
+
+    public float maxDistanceToBoss;
 
     void LateUpdate()
     {
-        if (target)
+        target.x = (enemyTransform.position.x + playerTransform.position.x) / 2;
+        target.y = (enemyTransform.position.y + playerTransform.position.y) / 2;
+        if (target != Vector3.zero)
         {
             Vector3 from = transform.position;
-            Vector3 to = target.position;
+            Vector3 to = target;
             to.z = transform.position.z;
 
+            Vector3 connectionVector = enemyTransform.position - playerTransform.position;
+            distanceBetweenTargets = Mathf.Sqrt((connectionVector.x * connectionVector.x) + (connectionVector.y * connectionVector.y) + (connectionVector.z * connectionVector.z));
+
+            if (distanceBetweenTargets >= maxDistanceToBoss)
+            {
+                to = playerTransform.position;
+                to.z = transform.position.z;
+            }
+                
+
+
             transform.position -= (from - to) * dampTime * Time.deltaTime;
+            //Camera.main.orthographicSize = distanceBetweenTargets;
+            
         }
 
         transform.position = new Vector3(
@@ -26,5 +47,12 @@ public class CameraFollow : MonoBehaviour
            Mathf.Clamp(transform.position.y, levelBottomRight.y + cameraClipOffsetY, levelTopLeft.y - cameraClipOffsetY),
            transform.position.z
        );
+        Debug.Log(distanceBetweenTargets);
+        Camera.main.orthographicSize = Mathf.Clamp(distanceBetweenTargets, minDistanceZ, maxDistanceZ);
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawSphere(target, 1);
     }
 }
