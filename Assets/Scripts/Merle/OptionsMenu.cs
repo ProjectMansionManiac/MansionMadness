180e7cdc5741b67314b445e9a205719ea472a52c
Helero <con-obi-chan@web.de>
2018-08-02 17:36:15 +0200

    more sounds
    [+]added sounds
    [+]added options menu script 1
    [-]deleted unused assets
    [*]changed some of the folder structure

---
+++ Assets/Scripts/Merle/OptionsMenu.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class OptionsMenu : MonoBehaviour {
+
+    //public AudioMixer audioMixer;
+    //public Dropdown resolutionDropdown;
+    Resolution[] resulutions;
+
+
+    /*void Start()
+    {
+       resulutions = Screen.resolutions;
+        resolutionDropdown.ClearOptions();
+
+        List<string> options = new List<string>();
+
+        int currentResulutionIndex = 0;
+
+        for (int i = 0; i < resulutions.Length ; i++)
+        {
+            string option = resulutions[i].width + "x" + resulutions[i].height;
+            options.Add(option);
+            if (resulutions[i].width == Screen.currentResolution.width && resulutions[i].height == Screen.currentResolution.height)
+            {
+
+                currentResulutionIndex = i;
+            }
+        }
+
+        resolutionDropdown.AddOptions(options);
+        resolutionDropdown.value = currentResulutionIndex;
+        resolutionDropdown.RefreshShownValue();
+
+    }
+    public void SetResolution(int resolutionIndex)
+    {
+        Resolution resolution = resulutions[resolutionIndex];
+        Screen.SetResolution(resolution.width, resolution.height , Screen.fullScreen);
+
+    }
+    
+    public void SetVolume(float volume)
+    {
+
+        audioMixer.SetFloat("volume", volume);
+    }
+    public void GraficRes (int qualityIndex)
+    {
+
+        QualitySettings.SetQualityLevel(qualityIndex);
+    }
+
+    */
+    public void SetFullscreen (bool isFullscreen)
+    {
+        Screen.fullScreen = isFullscreen;
+
+    }
+
+}
